4c05505b65a72e9b87375cdea3d264a250b00773
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2024-05-22 17:23:37 +0900

    a

--- src/utils/createMaze.ts
+++ src/utils/createMaze.ts
@@ -1,3 +1,5 @@
+//https://algoful.com/Archive/Algorithm/MazeExtend
+
 export const createMaze = (rows: number, cols: number) => {
   if (rows < 3) rows = 3;
   if (cols < 3) cols = 3;
@@ -11,11 +13,58 @@ export const createMaze = (rows: number, cols: number) => {
   const stack: [number, number][] = [];
   Array.from({ length: (rows - 1) / 2 }).forEach((_, i) =>
     Array.from({ length: (cols - 1) / 2 }).forEach((_, j) =>
-      stack.push([i + 1, j + 1])
+      stack.push([i * 2 + 1, j * 2 + 1])
     )
   );
 
-  document.body.innerHTML = JSON.stringify(stack);
+  while (stack.length > 0) {
+    const pos = stack.pop()!;
+    let x = pos[0];
+    let y = pos[1];
+    if (maze[x][y] !== 0) continue;
 
-  return maze;
+    maze[x][y] = 2;
+
+    const d = [
+      [-1, 0], //top
+      [0, -1], //left
+      [0, 1], //bottom
+      [1, 0], //right
+    ].sort(() => Math.random() - 0.5);
+
+    for (const i in d) {
+      const dx = d[i][0];
+      const dy = d[i][1];
+      let nx = x + dx;
+      let ny = y + dy;
+      if (nx < 0 || ny < 0 || nx >= rows || ny >= cols) continue;
+      if (maze[nx][ny] !== 0) continue;
+      let nnx = nx + dx;
+      let nny = ny + dy;
+      if (nnx < 0 || nny < 0 || nnx >= rows || nny >= cols) {
+        maze[nx][ny] = 1;
+        break;
+      }
+      if (maze[nnx][nny] !== 0) continue;
+      maze[nx][ny] = 1;
+      stack.push([nnx, nny]);
+      break;
+    }
+  }
+
+  const wallMaze: number[][] = [];
+  for (let i = 0; i < rows; i++) {
+    if (i % 2 !== 0) continue;
+    const rowArr = [];
+    for (let l = 0; l < cols; l++) {
+      if (l % 2 !== 0) continue;
+      let cell = 0;
+      if (i + 1 !== rows && maze[i + 1][l] !== 0) cell += 1;
+      if (l + 1 !== cols && maze[i][l + 1] !== 0) cell += 2;
+      rowArr.push(cell);
+    }
+    wallMaze.push(rowArr);
+  }
+
+  return { maze, wallMaze };
 };
--- test/index.ts
+++ test/index.ts
@@ -1,16 +1,41 @@
 import { createMaze } from "../src/utils/createMaze";
 
-const maze = createMaze(5, 5);
+const { maze, wallMaze } = createMaze(7, 7);
 
 const m = document.createElement("main");
 maze.forEach((v) => {
   const d = document.createElement("div");
   v.forEach((c) => {
     const e = document.createElement("span");
-    if (c >= 1) e.style.borderRightColor = "red";
-    if (c >= 2) e.style.borderBottomColor = "red";
+    e.className = "cell";
+    if (c >= 1) e.style.backgroundColor = "red";
+    if (c >= 2) e.style.backgroundColor = "yellow";
+    if (c >= 3) e.style.backgroundColor = "blue";
+    if (c >= 4) e.style.backgroundColor = "green";
     d.appendChild(e);
   });
   m.appendChild(d);
 });
+
 document.body.appendChild(m);
+
+const m2 = document.createElement("main");
+
+wallMaze.forEach((v) => {
+  const d = document.createElement("div");
+  v.forEach((c) => {
+    const e = document.createElement("span");
+    e.className = "cell2";
+    if (c === 1) e.style.borderBottomColor = "red";
+    if (c === 2) e.style.borderRightColor = "red";
+    if (c === 3) {
+      e.style.borderBottomColor = "red";
+      e.style.borderRightColor = "red";
+    }
+
+    d.appendChild(e);
+  });
+  m2.appendChild(d);
+});
+
+document.body.appendChild(m2);
